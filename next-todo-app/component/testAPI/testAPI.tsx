6ff38233a7be1457a836552348ac83bbe588724c
avimehenwal <avi.mehanwal@gmail.com>
2020-12-22 00:55:03 +0100

    style(typescript): components

---
+++ next-todo-app/component/testAPI/testAPI.tsx
@@ -0,0 +1,12 @@
+// importing FunctionComponent
+// import React, { FunctionComponent } from 'react';
+
+const TestApi = () => {
+  return (
+    <div>
+      <h3>Test API</h3>
+    </div>
+  )
+}
+
+export default TestApi
---
+++ next-todo-app/pages/game.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// @ts-ignore: Module not found
+import TestApi from '../component/testAPI'
+
+const gamePage = () => {
+  return (
+    <div>
+      Game Page
+      <TestApi></TestApi>
+    </div>
+  )
+}
+
+export default gamePage
