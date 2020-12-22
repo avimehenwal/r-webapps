8a049d08906dc867205f903602a4b6936992abbd
avimehenwal <avi.mehanwal@gmail.com>
2020-12-22 01:27:03 +0100

    feat(react): Objects in useState hook

--- next-todo-app/component/testAPI/testAPI.tsx
+++ next-todo-app/component/testAPI/testAPI.tsx
@@ -1,10 +1,36 @@
 // importing FunctionComponent
-// import React, { FunctionComponent } from 'react';
+import React, { useState, useEffect, FunctionComponent } from 'react';
+import axios from 'axios';
 
 const TestApi = () => {
+  const [data, setData] = useState({key: 'value'})
+
+  useEffect(async() => {
+    const result = await axios('http://localhost:3000/api/hello')
+    setData(result.data)
+  }, [])
+
   return (
     <div>
       <h3>Test API</h3>
+      <pre>
+        {JSON.stringify(data, null, 4)}
+      </pre>
+
+      <table>
+        <tr>
+          <th>S.No</th>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+        {Object.entries(data).map(([key, value], index) =>
+        <tr key={key}>
+          <td>{index}</td>
+          <td>{key}</td>
+          <td>{value}</td>
+        </tr>
+      )}
+    </table>
     </div>
   )
 }
