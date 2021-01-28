2dc4e8b661c5589f3aa352ece002c46889bb0cfd
avimehenwal <avi.mehanwal@gmail.com>
2021-01-28 01:56:44 +0100

    test(cra): :sparkles: use component in cra

---
+++ component-lib/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Card } from './components/FooComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Card title="Welcome! Pello" paragraph="To this example! fucker" />
+    </div>
+  );
+}
+
+export default App;
